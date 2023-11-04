bc3739bdf958c7dac167b4f7dbe224126b9e0707
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 09:13:34 +0000

    conversionofdatatypes

---
+++ .devcontainer/basics01/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let number = Number(score)
+console.log(typeof number)
+console.log(number)
+
+// "33" -> 33
+// "33abc" -> Nan
+// true -> 1 , false -> 0
+
+let isloggedin = 0;
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+// 1 -> True
+// 0 -> False
+// "" ->False
+// "Tanish" ->True
+
+let sumnumber = 33
+let stringnumber = String(sumnumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)

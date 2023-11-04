907bfeb12c9c8b2bd55a9b4258ed89e496b56346
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 10:02:31 +0000

    comparison

--- .devcontainer/basics01/04_comparison.js
+++ .devcontainer/basics01/04_comparison.js
@@ -3,16 +3,16 @@
 // console.log(1!=0)
 // console.log(1==7)
 
-// console.log("2">1) // -> True
-// console.log("02">1) // -> True
+ console.log("2">1) // -> True
+ console.log("02">1) // -> True
 
-// console.log(null>0) // -> False
-// console.log(null==0) // -> False
-// console.log(null>=0) // -> True
+ console.log(null>0) // -> False
+ console.log(null==0) // -> False
+ console.log(null>=0) // -> True
 
-// console.log(undefined>0) //-> False
-// console.log(undefined<0) // -> False
-// console.log(undefined==0) // ->False
+ console.log(undefined>0) //-> False
+ console.log(undefined<0) // -> False
+ console.log(undefined==0) // ->False
 
-// console.log("2"==2) // ->True
+ console.log("2"==2) // ->True
 console.log("2"===2) // -> not correct because "2" is String and 2 is number ->False

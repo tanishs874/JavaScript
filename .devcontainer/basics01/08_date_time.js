aa28c201f4a24285ff163fbde64acf92e727f168
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 15:31:24 +0000

    date and time

---
+++ .devcontainer/basics01/08_date_time.js
@@ -0,0 +1,22 @@
+// let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(typeof mydate)
+
+let mycreateddate = new Date(2023,0,23)
+console.log(mycreateddate.toDateString())
+
+// let createdDate = new Date(2023,0,23,55,8)
+// let createdDate = new Date("2023-01-14")
+// let createdDate = new Date("01-14-2023")
+console.log(createdDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));

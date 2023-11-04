82907f36abd617de143341e70bd2165cf2158c48
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 09:37:36 +0000

    conversionoperations

--- .devcontainer/basics01/03_conversionOperations.js
+++ .devcontainer/basics01/03_conversionOperations.js
@@ -23,3 +23,33 @@ let sumnumber = 33
 let stringnumber = String(sumnumber)
 console.log(stringnumber)
 console.log(typeof stringnumber)
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+*/
+
+let str1 = "Tanish"
+let str2 = " Sharma"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2) // ->12
+console.log(1+"2") // ->12
+console.log("1"+ 2 + 2) // ->122
+console.log(1 + 2 + "2") // ->32
+
+console.log(true) // ->true
+console.log(+true) // -> 1
+console.log(+"") // -> 0
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter)

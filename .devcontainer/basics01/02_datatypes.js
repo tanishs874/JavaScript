fd1ed29f7988c4b560e03224b9b453798c52887a
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 08:53:45 +0000

    datatypes

---
+++ .devcontainer/basics01/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all Js code as newer version..
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+
+    3)//code readability should be high..
+console.log("tanish")
+
+let name = "Tanish" //String
+let age = 20 //number
+let isloggedin = True //boolean
+
+/* JavaScript has 8 Datatypes
+ 1. String ->""
+ 2. Number -> 2 to power 53
+ 3. Bigint
+ 4. Boolean -> True/False
+ 5. Undefined -> Value not assigned ->Type of is undefined
+ 6. Null -> StandAlone value ->Type of is object
+ 7. Symbol -> Unique
+ 8. Object 
+ */

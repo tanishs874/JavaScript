54577fe1d6374d261f3e2e6edd50d53d58d1eb33
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 11:57:17 +0000

    datatypes

---
+++ .devcontainer/basics01/05_datatypesSummary.js
@@ -0,0 +1,34 @@
+//Types of data
+/*
+Primitive data type -> String,number,bigint,boolean,null,undefined,symbol,object
+Non-Primitive data type -> Array,object,Functinos
+*/
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);//->type of is Symbol

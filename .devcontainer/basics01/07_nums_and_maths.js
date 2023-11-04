3cacfcb1317fd5b212416c735b9201d3dccf607c
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-04 13:18:48 +0000

    nums and math in java

---
+++ .devcontainer/basics01/07_nums_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// const balance = new Number(100)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const anothernumber = 123.899
+// console.log(anothernumber.toPrecision(3))//->It is like roundoff value
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+//++++++++++++Maths+++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(25))
+// console.log(Math.min(2,3,4,5,6,7,77))
+// console.log(Math.max(5,4,6,4,6,4,3,55))
+
+
+console.log((Math.floor(Math.random()*10)+1))
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)

449ad406ea1fe041663e472bc8b1427602cdf928
tanishs874 <101451522+tanishs874@users.noreply.github.com>
2023-11-07 06:04:35 +0000

    json

---
+++ .devcontainer/basics02/05_objects.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename:"js in hindi",
+    price:"100",
+    courseInstructor:"Tanish"
+}
+ 
+// course.courseInstructor
+
+const{courseInstructor:inst}=course
+// console.log(courseInstructor)
+console.log(inst)
+
+// {
+//     "name":"Tanish",
+//     "coursename":"js in hindi",
+//     "price":"Free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
